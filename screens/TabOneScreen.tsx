d1d5e5357848dfd966c1b78924f28fca5267bc66
Chriscbr <crybicki98@gmail.com>
2021-04-07 18:38:48 -0400

    add placeholder title and buttons

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +7,26 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Patient Portal</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.body}>What would you like help with today?</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Request medication refill</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Request physical therapy</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Check lab results</Text>
+      </TouchableOpacity>
+      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
   );
 }
@@ -21,12 +38,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 20,
   },
   separator: {
     marginVertical: 30,
-    height: 1,
+    height: 0,
     width: '80%',
   },
+  button: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 40,
+    width: 300,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#000',
+  }
 });
