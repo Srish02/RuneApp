21ade7ea4a9dfec5bc48875e2c1cfffd16aa6eb6
Chriscbr <crybicki98@gmail.com>
2021-05-01 17:32:16 -0400

    Format REST API data as list

--- screens/PatientListScreen.tsx
+++ screens/PatientListScreen.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     fetch('https://rune-rest-api.azurewebsites.net/api/patients')
       .then((response) => response.json())
-      .then((json) => {setData(json[0])}) // first patient
+      .then((json) => {
+        console.log(json);
+        setData(json)
+      })
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
@@ -21,7 +24,14 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Patient List</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {!isLoading && <Text>{JSON.stringify(data)}</Text>}
+      {isLoading ? <Text>Loading...</Text> :
+      <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.id}, {item.FirstName}, {item.LastName}</Text>
+          )}
+        />}
     </View>
   );
 }
