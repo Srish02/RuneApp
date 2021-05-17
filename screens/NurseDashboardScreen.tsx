1efafbc174eb27f2a55498b3c0b393461d78cf8d
Chriscbr <crybicki98@gmail.com>
2021-05-17 01:57:08 -0400

    Clean up CSS

--- screens/NurseDashboardScreen.tsx
+++ screens/NurseDashboardScreen.tsx
@@ -5,7 +5,6 @@ import { FlatList, StyleSheet, TouchableOpacity, Image, View } from 'react-nativ
 
 import { Text } from '../components/Themed';
 import { RootStackParamList } from '../types';
-import * as Notifications from 'expo-notifications';
 
 export default function NurseDashboardScreen({ route, navigation }: StackScreenProps<RootStackParamList, 'NurseDashboard'>) {
   const [isLoading, setLoading] = useState(true);
@@ -24,24 +23,29 @@ export default function NurseDashboardScreen({ route, navigation }: StackScreenP
 
   return (
     <View style={styles.container}>
-      {/* <Text style={[styles.title, styles.setColorBlack]}>Patient List</Text> */}
-      {/* <Image style={styles.tinyLogo} source={require('@expo/snack-static/react-native-logo.png')} /> */}
-      <Image
-        style={styles.profilepicture}
-        source={{
-          uri:
-            'https://runeimages.blob.core.windows.net/assets/andrea.png',
-        }}
-      />
+      <View style={{ flexDirection: 'row', padding: 30, marginTop: 50 }}>
+        <View style={{ flexDirection: 'column', flex: 2 }}>
+          <Text style={styles.goodMorning}>Good morning,</Text>
+          <Text style={styles.goodMorningName}>Olivia</Text>
+        </View>
+        <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'center' }}>
+          <Image
+            style={styles.profilepicture}
+            source={{
+              uri:
+                'https://runeimages.blob.core.windows.net/assets/andrea.png',
+            }}
+          />
+        </View>
+      </View>
 
-      <Text style={styles.goodMorning}>Good morning,</Text>
-      <Text style={styles.goodMorningName}>Olivia</Text>
       <View style={styles.separator} />
       <Text style={styles.patientAssigned}>Patients Assigned:</Text>
       {isLoading ? <Text>Loading...</Text> :
         <FlatList
           data={data}
           keyExtractor={({ id }, index) => id}
+          style={{ margin: 20 }}
           renderItem={({ item }) => (
             //  <Text style={styles.setColorBlack}>{item.id}, {item.FirstName}, {item.token}</Text>
             <TouchableOpacity
@@ -73,7 +77,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 120,
+    marginVertical: 20,
     height: 2,
     width: '80%',
   },
@@ -94,28 +98,23 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   profilepicture: {
-    right: -120,
-    top: 50,
+    alignSelf: 'center',
     width: 70,
     height: 70,
     borderRadius: 150 / 2,
   },
   goodMorning: {
-    left: "-11.5%",
-    top: 60,
     fontSize: 33,
     color: '#000000'
   },
   goodMorningName: {
-    left: "-26%",
-    top: 65,
     fontSize: 33,
     fontWeight: "bold",
     color: '#000000'
   },
   patientAssigned: {
-    marginTop: -120,
-    left: "-16%",
+    alignSelf: 'flex-start',
+    marginLeft: 50,
     fontSize: 20,
     color: '#000000'
   },
