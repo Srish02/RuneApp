52122ab6e9c7655b7746226f86f5437d76207ea0
Richard Wang <rlw324@cornell.edu>
2021-05-17 04:15:26 -0400

    added pdf viewer

--- screens/PatientTestResultsScreen.tsx
+++ screens/PatientTestResultsScreen.tsx
@@ -6,10 +6,19 @@ import { Text, View } from '../components/Themed';
 import Colors from '../constants/Colors';
 import { RootStackParamList } from '../types';
 
+import PDFReader from 'rn-pdf-reader-js'
+
 export default function PatientTestResults({ route, navigation }: StackScreenProps<RootStackParamList, 'PatientTestResults'>) {
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Patient Test Results</Text>
+      <PDFReader
+        source={{
+          uri: 'https://runefilestorage.blob.core.windows.net/tests/labresult.pdf',
+        }}
+        style={styles.pdf}
+      />
       <TouchableOpacity
         onPress={() => navigation.goBack()}
         style={styles.button}>
@@ -27,8 +36,10 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.patient.background,
   },
   title: {
+    top: 30,
     fontSize: 20,
     fontWeight: 'bold',
+    color: "blue"
   },
   separator: {
     marginVertical: 30,
@@ -47,5 +58,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     color: '#000',
+  },
+  pdf: {
+    width: 400,
   }
 });
